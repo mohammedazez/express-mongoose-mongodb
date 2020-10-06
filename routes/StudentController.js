e2f17d11a860687628d326d5c872c3471b52c450
azezmohammed <muhamadaziz047@gmail.com>
2020-10-06 22:37:10 +0700

    Add delete and update for management data

--- routes/StudentController.js
+++ routes/StudentController.js
@@ -4,7 +4,9 @@ const express = require("express");
 const Student = require("../models/Student");
 const app = express();
 
-// Read by all
+// Dokumentasi lengkap tentang crud
+// https://mongoosejs.com/docs/queries.html
+// Read all data
 app.get("/student", async (req, res) => {
   console.log("Ini Student", Student);
   const students = await Student.find({});
@@ -37,6 +39,30 @@ app.post("/student", async (req, res) => {
     res.send(students);
   } catch (error) {
     console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+// Delete by id
+app.delete("/student/:id", async (req, res) => {
+  const students = await Student.findByIdAndDelete(req.params.id);
+
+  try {
+    if (!students) res.status(404).send("Tidak ditemukan");
+    res.status(200).send("Data berhasil dihapus");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+// Update data by id
+app.put("/student/:id", async (req, res) => {
+  const students = await Student.findByIdAndUpdate(req.params.id, req.body);
+  try {
+    await students.save();
+    res.status(200).send("data berhasil dirubah");
+  } catch (error) {
     res.status(500).send(error);
   }
 });
